93ed6283bc13e12f73d03b73b36b2d81165601b4
naufal-ihsan <111897846+naufal-ihsan@users.noreply.github.com>
2025-05-13 00:18:10 +0700

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const db = require('./config/firebaseConfig');
+const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // penting agar bisa baca body JSON
+
+app.use('/api/auth', authRoutes); // routing auth
+
 app.get('/', (req, res) => {
-    res.send('NutriSmart Backend is Running');
+  res.send('Giziku Backend is Running');
 });
 
 app.listen(PORT, () => {
-    console.log("Server is running on port ${PORT}");
+  console.log(`Server is running on port ${PORT}`);
 });
